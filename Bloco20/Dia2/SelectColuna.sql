3d19451580a02a56ab301df01eeb9fe62e7271eb
Galaraz <jvictor.si.2013@gmail.com>
2021-02-26 11:58:18 -0300

    dia 2 ex2

---
+++ Bloco20/Dia2/SelectColuna.sql
@@ -0,0 +1,18 @@
+1-Escreva uma query que selecione todas as colunas da tabela city
+
+SELECT * FROM SAKILA.CITY;
+2-Escreva uma query que exiba apenas coluna first_name , last_name da tabela customer
+
+SELECT FIRST_NAME, LAST_NAME FROM SAKILA.CUSTOMER;
+
+3-Escreva uma query que exiba todas as colunas da tabela rental
+
+SELECT * FROM SAKILA.RENTAL;
+
+4-Escreva uma query que exiba o título , descrição e data de lançamento dos filmes registrados na tabela Filme
+
+SELECT TITLE, DESCRIPTION, RELEASE_YEAR FROM SAKILA.FILM;
+
+5-Explore, usando o select, todas as tabelas do banco de dados
+
+SELECT *  FROM SAKILA.
