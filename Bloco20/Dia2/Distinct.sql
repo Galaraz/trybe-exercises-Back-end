cb08c15112ed157f613c8deb9349c2d196969ed1
Galaraz <jvictor.si.2013@gmail.com>
2021-02-26 12:08:32 -0300

    dia 2: ex3 e 4

---
+++ Bloco20/Dia2/Distinct.sql
@@ -0,0 +1,19 @@
+1-Monte uma query para encontrar somente os nomes e idades , ambos únicos
+
+SELECT DISTINCT Nome, Idade FROM Alunos;
+
+2-Quantas linhas vocês encontraria no query anterior? 4
+
+
+3-Monte uma query para encontrar somente os nomes únicos
+SELECT DISTINCT Nome FROM Alunos;
+
+4-Quantas linhas vocês encontraria no query anterior? 4
+
+5-Monte uma query para encontrar somente as idades únicas
+
+SELECT DISTINCT Idade FROM Alunos;
+
+6- Quantas linhas vocês encontraria no query anterior? 4
+
+
