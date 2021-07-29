2fd1eb749c61194ee73d909792260f1ae1427998
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-07-29 00:09:18 -0300

    dia 3

---
+++ Bloco-30/Dia-03/aulaAovivo/controllers/index..js
@@ -0,0 +1,11 @@
+const ping = require('./ping');
+const users = require('./users');
+const products = require('./products');
+const selloffs = require('./selloffs');
+
+module.exports = {
+  ping,
+  users,
+  products,
+  selloffs,
+};
